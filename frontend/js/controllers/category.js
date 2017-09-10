f45e3a61841f2399ff49fc018fcbff4394cdc9e6
navinnarshetty <navinnarshetty@gmail.com>
2017-09-10 14:28:51 +0530

    category page structure done

--- frontend/js/controllers/category.js
+++ frontend/js/controllers/category.js
@@ -1,11 +1,38 @@
-myApp.controller('CategoryCtrl', function ($scope, TemplateService, NavigationService, $timeout, $uibModal,$http) {
+myApp.controller('CategoryCtrl', function ($scope, TemplateService, NavigationService, $timeout, $uibModal, $http) {
   $scope.template = TemplateService.getHTML("content/category.html");
   TemplateService.title = "Home"; //This is the Title of the Website
   $scope.navigation = NavigationService.getNavigation();
   $scope.data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  $scope.slider={};
-  $scope.slider.count=0;
-  
+  $scope.data1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+  $scope.slider = {};
+  $scope.slider.count = 0;
+
+  $scope.Productlist = [{
+    img: "https://s3.cashify.in/cashify/productLine/img/xxhdpi/587753dd18df2.png",
+    name: "mobile Phone",
+    id: "1"
+  }, {
+    img: "https://s3.cashify.in/cashify/productLine/img/xxhdpi/587753dd18df2.png",
+    name: "mobile Phone",
+    id: "2"
+  }, {
+    img: "https://s3.cashify.in/cashify/productLine/img/xxhdpi/587753dd18df2.png",
+    name: "mobile Phone",
+    id: "3"
+  }, {
+    img: "https://s3.cashify.in/cashify/productLine/img/xxhdpi/587753dd18df2.png",
+    name: "mobile Phone",
+    id: "4"
+  }, {
+    img: "https://s3.cashify.in/cashify/productLine/img/xxhdpi/587753dd18df2.png",
+    name: "mobile Phone",
+    id: "5"
+  }, {
+    img: "https://s3.cashify.in/cashify/productLine/img/xxhdpi/587753dd18df2.png",
+    name: "mobile Phone",
+    id: "6"
+  }]
+
   $scope.nextSlide = function () {
     console.log($scope.slider.count, "data passed");
     ++$scope.slider.count;
@@ -14,32 +41,30 @@ myApp.controller('CategoryCtrl', function ($scope, TemplateService, NavigationSe
     $scope.viewslide($scope.slider.count);
   }
 
+  $scope.prevSlide = function () {
+    --$scope.slider.count;
+    $scope.viewslide($scope.slider.count);
+  }
+
   $scope.viewslide = function (data) {
-    var val=data;
+    var val = data;
     console.log("in switch")
     console.log(val);
-    if(val<=3){
-    $scope.slideView=$scope.slider.count;
-    console.log("i am in ",$scope.slideView);
+    if (val <= 3) {
+      $scope.slideView = $scope.slider.count;
+      console.log("i am in ", $scope.slideView);
     }
 
-    // switch (val) {
-    //   case 0:
-    //     $scope.slideView = 0;
-    //     console.log("i am in ",$scope.slideView);
-    //     break;
-    //   case 1:
-    //     $scope.slideView = 1;
-    //     console.log("i am in ",$scope.slideView);
-    //     // console.log($agentloginView)
-    //     break;
-    //   case 2:
-    //     $scope.slideView = 2;
-    //     console.log("i am in ",$scope.slideView);
-    //     break;
-
-    // }
   }
+
+  $scope.showRam = function () {
+    $scope.showramSize = !$scope.showramSize;
+  }
+
+  $scope.question = function () {
+    $scope.questionHolder = !$scope.questionHolder;
+  }
+
   //switching between cards ends
 
 })
--- frontend/js/controllers/header-controller.js
+++ frontend/js/controllers/header-controller.js
@@ -1,7 +1,21 @@
 myApp.controller('headerCtrl', function ($scope, TemplateService, $uibModal) {
     $scope.template = TemplateService;
-    $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-        $(window).scrollTop(0);
-    });
-    $.fancybox.close(true);
+    // $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+    //     $(window).scrollTop(0);
+    // });
+    // $.fancybox.close(true);
+
+    // var loginmodal;
+    // $scope.login = function () {
+    //     console.log("modal")
+    //     loginmodal = $uibModal.open({
+    //         animation: true,
+    //         scope: $scope,
+    //         backdrop: 'static',
+    //         keyboard: false,
+    //         templateUrl: 'views/modal/login.html',
+    //         size: 'md',
+    //         // windowClass: 'backmodal'
+    //     })
+    // }
 });
